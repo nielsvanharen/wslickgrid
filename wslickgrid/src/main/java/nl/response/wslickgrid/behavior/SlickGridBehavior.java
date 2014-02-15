ce2ee9d2189b7bf28ea29710c9126d6209bcfea5
nielsvanharen <niels@response.nl>
2014-02-15 16:52:40 +0100

    initial commit

---
+++ wslickgrid/src/main/java/nl/response/wslickgrid/behavior/SlickGridBehavior.java
@@ -0,0 +1,12 @@
+package nl.response.wslickgrid.behavior;
+
+import org.apache.wicket.behavior.Behavior;
+
+public class SlickGridBehavior extends Behavior{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
